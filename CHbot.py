8984ebaa3ac91d6bb6c622d700d254a19e5ab73d
@AmKuSaL <78016063+kusalCY@users.noreply.github.com>
2021-04-19 20:56:11 +0530

    Update CHbot.py

--- CHbot.py
+++ CHbot.py
@@ -170,7 +170,7 @@ if Config.AUDIO_THUMBNAIL == "No":
        # info (s2tw)
        webpage_url = info_dict['webpage_url']
        title = s2tw(info_dict['title'])
-       caption = f"<b>@Cyber0Hacker<a href=\"{webpage_url}\">{title}</a></b>"
+       caption = f"<b>@Cyber0Hacker <a href=\"{webpage_url}\">{title}</a></b>"
        duration = int(float(info_dict['duration']))
        performer = s2tw(info_dict['uploader'])
        await message.reply_audio(audio_file, caption=caption, duration=duration,
@@ -193,7 +193,7 @@ else:
        # info (s2tw)
        webpage_url = info_dict['webpage_url']
        title = s2tw(info_dict['title'])
-       caption = f"<b>@Cyber0Hacker<a href=\"{webpage_url}\">{title}</a></b>"
+       caption = f"<b>@Cyber0Hacker <a href=\"{webpage_url}\">{title}</a></b>"
        duration = int(float(info_dict['duration']))
        performer = s2tw(info_dict['uploader'])
        await message.reply_audio(audio_file, caption=caption, duration=duration,
@@ -243,7 +243,7 @@ if Config.VIDEO_THUMBNAIL == "No":
       # info (s2tw)
       webpage_url = info_dict['webpage_url']
       title = s2tw(info_dict['title'])
-      caption = f"<b>@Cyber0Hacker<a href=\"{webpage_url}\">{title}</a></b>"
+      caption = f"<b>@Cyber0Hacker <a href=\"{webpage_url}\">{title}</a></b>"
       duration = int(float(info_dict['duration']))
       width, height = get_resolution(info_dict)
       await message.reply_video(
@@ -263,7 +263,7 @@ else:
       # info (s2tw)
       webpage_url = info_dict['webpage_url']
       title = s2tw(info_dict['title'])
-      caption = f"<b>@Cyber0Hacker<a href=\"{webpage_url}\">{title}</a></b>"
+      caption = f"<b>@Cyber0Hacker <a href=\"{webpage_url}\">{title}</a></b>"
       duration = int(float(info_dict['duration']))
       width, height = get_resolution(info_dict)
       await message.reply_video(
