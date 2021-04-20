35402bc27d2b3028867a19d520527abfe22497bc
@AmKuSaL <78016063+kusalCY@users.noreply.github.com>
2021-04-20 06:40:14 +0530

    Update CHbot.py

--- CHbot.py
+++ CHbot.py
@@ -243,7 +243,7 @@ if Config.VIDEO_THUMBNAIL == "No":
       # info (s2tw)
       webpage_url = info_dict['webpage_url']
       title = s2tw(info_dict['title'])
-      caption = f"<b>@Cyber0Hacker</b> <b><a href=\"{webpage_url}\">{title}</a></b>"
+      caption = f"<b>@Cyber0Hacker</b> <b><a> href=\"{webpage_url}\">{title}</a></b>"
       duration = int(float(info_dict['duration']))
       width, height = get_resolution(info_dict)
       await message.reply_video(
