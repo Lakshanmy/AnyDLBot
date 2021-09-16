e250a2d80d8831d23054350888b5a76f0df4db20
Lakshan <78016063+Lakshanmy@users.noreply.github.com>
2021-09-16 12:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1858870769:AAEJNQHDn2P7NKcJESEFGDwwyGO-iZwxjfY")
 
-    APP_ID = int(os.environ.get("APP_ID", 3413890))
+    APP_ID = int(os.environ.get("APP_ID", 2987477))
 
-    API_HASH = os.environ.get("API_HASH", "4d309f9bb03ed141f5528b32f7866ad3")
+    API_HASH = os.environ.get("API_HASH", "fab0ac420d45ae0aea6be644017b5c4e")
 
     AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "https://telegra.ph/file/d2372fe42ccf8c9273088.jpg")
 
